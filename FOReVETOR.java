b7a4d9d2795e3a1c43fb9a859a0598fb8cc82f64
Andson <167490002+andsonqueiroz@users.noreply.github.com>
2024-08-11 11:21:08 -0300

    Add files via upload

---
+++ FOReVETOR.java
@@ -0,0 +1,23 @@
+// Andson de Queiroz Domingues, 3ADS
+
+import java.util.Scanner;
+public class FOReVETOR{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in); // DECLARAÇÃO DO SCANNER COM NOME "ler"
+        float[] notas = new float[5];       // DECLARANDO VETOR COM TAMANHO 5.
+                                        // PARA DECLARAR UM VETOR, PRECISA SEGUIR A SINTAXE "TIPO[] nome = new TIPO[tamanho]"
+        float media = 0;
+
+        for(int i=0; i<5; i++){     // PARA FAZER UM for, SEGUE A MESMA LÓGICA DAS OUTRAS LINGUAGENS.
+            System.out.println("Digite a nota " + (i+1));
+            notas[i] = ler.nextFloat();
+        }
+        for(int i = 0; i<5; i++){
+            media = media + notas[i];
+        }
+
+        media = media / 5;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
